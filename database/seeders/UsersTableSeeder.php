e8e2fd57802a0953977f3ef2c50a97760d81cf0c
homero507 <serranior@gmail.com>
2021-07-16 16:18:35 -0400

    #4, #5 seeder tables article y users listos

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'Adminprueba@example.com',
+            'password' => 15963,
+        ]);
+
+        $password = Hash::make('yourPa$$w0rd');
+
+        for ($i=0; $i < 10 ; $i++) { 
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => Carbon::now(),
+                'password' => $password,
+
+            ]);
+        }
+    }
+}
